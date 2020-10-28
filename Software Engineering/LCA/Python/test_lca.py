0f246211f679e6fd218ad26f9949b8bcd28c0f94
lukemcgrath98 <60216801+lukemcgrath98@users.noreply.github.com>
2020-10-28 00:03:49 +0000

    Update test_lca.py

--- Software Engineering/LCA/Python/test_lca.py
+++ Software Engineering/LCA/Python/test_lca.py
@@ -13,6 +13,10 @@ class TestLCA(TestCase):
         self.assertEqual(getPath(root, path, 7), False)
 
     # find the path from the root back to itself
+        path = []
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
     def test_PathRoot(self):
         path = []
         root = Node(1)
@@ -20,10 +24,6 @@ class TestLCA(TestCase):
 
     # test on a non-empty tree with two different nodes
     def test_findPath(self):
-        path = []
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(3)
         root.left.left = Node(4)
         root.left.right = Node(5)
         root.right.left = Node(6)
@@ -52,15 +52,3 @@ class TestLCA(TestCase):
         root = Node(1)
         path = []
         self.assertEqual(findLCA(root, 1, 1), 1)
-
-    # test for complete tree
-    def test_findLCA(self):
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(6)
-        root.left.left = Node(3)
-        root.left.left.right = Node(4)
-        root.left.left.right.right = Node(5)
-        root.left.right = Node(9)
-        root.right.right = Node(7)
-        root.right.right.right = Node(8)
