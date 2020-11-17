2343e899e69f5418cdfb9b39af0e987f9addac50
lukemcgrath98 <60216801+lukemcgrath98@users.noreply.github.com>
2020-11-17 20:37:36 +0000

    Add files via upload

---
+++ Functional Programming/Exercise 1/Ex01.hs
@@ -0,0 +1,74 @@
+module Ex01 where
+import Data.Char (toUpper)
+import Data.List (group)
+
+name, idno, username :: String
+name      =  "McGrath, Luke"  -- replace with your name
+idno      =  "17337376"    -- replace with your student id
+username  =  "mcgratlu"   -- replace with your TCD username
+
+
+declaration -- do not modify this
+ = unlines
+     [ ""
+     , "@@@ This exercise is all my own work."
+     , "@@@ Signed: " ++ name
+     , "@@@ "++idno++" "++username
+     ]
+
+
+{- Part 1
+
+Write a function 'raise' that converts a string to uppercase
+
+Hint: 'toUpper :: Char -> Char' converts a character to uppercase
+if it is lowercase. All other characters are unchanged.
+It is imported should you want to use it.
+
+-}
+raise :: String -> String
+raise = map toUpper
+
+{- Part 2
+
+Write a function 'nth' that returns the nth element of a list.
+Hint: the test will answer your Qs
+
+-}
+nth :: Int -> [a] -> a
+nth b (x:xs) 
+	| (b == 1) = x 
+	| otherwise = nth (b-1) xs
+
+
+{- Part 3
+
+Write a function `commonLen` that compares two sequences
+and reports the length of the prefix they have in common.
+
+-}
+commonLen :: Eq a => [a] -> [a] -> Int
+commonLen [] b = 0
+commonLen (x:rest) b =
+    let index = if x `elem` b then 1 else 0
+    in index + commonLen rest b
+
+{- Part 4
+
+(TRICKY!) (VERY!)
+
+Write a function `runs` that converts a list of things
+into a list of sublists, each containing elements of the same value,
+which when concatenated together give the same list
+
+So `runs [1,2,2,1,3,3,3,2,2,1,1,4]`
+ becomes `[[1],[2,2],[1],[3,3,3],[2,2],[1,1],[4]]`
+
+Hint:  `elem :: Eq a => a -> [a] -> Bool`
+
+HINT: Don't worry about code efficiency
+       Seriously, don't!
+
+-}
+runs :: Eq a => [a] -> [[a]]
+runs xs = group(xs)
